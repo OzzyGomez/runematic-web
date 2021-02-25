cd9a76d4ff2c6ab89ca81bb2e17f475f561e34b4
Osvaldo villagomez <osvaldovillagomez@Osvaldos-MBP.attlocal.net>
2021-02-24 19:40:27 -0600

    video and icons updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,22 @@ import './App.css';
 
 function App() {
   return (
-		<Fragment>
-	    <video autoPlay muted loop className="video">
-	      <source src="Assets/runematic_background_short.mp4" type="video/mp4" />
-	      Your browser does not support HTML5 video.
-			</video>
-
-	    <div className="content">
-				<div className="titlebox">
-	      	<h1 className="title">RUNEMATIC</h1>
-	      	<p className="subtitle">A Story Based Running App</p>
-					<p className="description">
-						Run to different stories that put you in the middle of the action
-						and get you motivated!
-					</p>
-					<div className="coming-soon">Coming Soon</div>
-				</div>
-	    </div>
+	<Fragment>
+		<video autoPlay muted loop className="video">
+			<source src="Assets/runematic_background.mp4" type="video/mp4" />
+			Your browser does not support HTML5 video.
+		</video>
+		<div className="content">
+			<div className="titlebox">
+				<h1 className="title">RUNEMATIC</h1>
+				<p className="subtitle">A Story Based Running App</p>
+				<p className="description">
+					Run to different stories that put you in the middle of the action
+					and get you motivated!
+				</p>
+				<div className="coming-soon">Coming Soon</div>
+			</div>
+		</div>
     </Fragment>
   );
 }
