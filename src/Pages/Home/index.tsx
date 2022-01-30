30cdd22b9f10c26858617be5fa236d07752b0b56
Osvaldo villagomez <osvaldovillagomez@MacBook-Pro.attlocal.net>
2022-01-29 18:29:36 -0600

    icons updated

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -7,15 +7,6 @@ const Home = () => {
 			<div className="content">
 				<div className={classes['video-background']}>
 					<div className={classes['video-foreground']}>
-						{/* <iframe
-							title="video"
-							width="560"
-							height="315"
-							src="https://www.youtube.com/embed/kmSD990jyII?controls=0&rel=0&autoplay=1&loop=1&mute=1&&showinfo=0&playlist=rIjg3qyDkQs"
-							frameBorder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-							allowFullScreen
-						/> */}
 						<iframe 
 							width="560"
 							 height="315" 
